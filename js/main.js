5a6c09dea03f26bfe9472d0b4f02f5863a4f9b7b
Ever <ever4ever@yandex.ru>
2021-08-26 13:54:14 +0300

    Переменным присвоены значения, реализоваы некоторые методы

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const money,
+let money,
     income,
     addExpenses,
     deposit,
@@ -9,3 +9,25 @@ const money,
 
 alert('Hello, World!');
 console.log('Hello, World!');
+
+money = 350000;
+income = 'Фриланс';
+addExpenses = 'Английский, фитнес, косметолог';
+deposit = true;
+mission = 3500000;
+period = 12;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+
+addExpenses = addExpenses.toLowerCase().split(', ');
+console.log(addExpenses);
+
+let budgetDay;
+budgetDay = money/30;
+console.log(budgetDay);
